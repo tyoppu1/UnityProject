cbdf2b78b8168b3bb1673929d6a91cde35a7fe8c
unknown <>
2016-07-04 02:50:47 +0900

    初コミット

---
+++ Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    Rigidbody rigid;
+
+    public float speed;
+
+    Vector3 movement;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+	
+
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigid.AddForce(movement * speed * Time.deltaTime);
+
+    }
+}
---
+++ Scripts/HitFlag.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitFlag : MonoBehaviour
+{
+
+    public bool cubeHit;
+    
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        cubeHit = true;
+    }
+}
